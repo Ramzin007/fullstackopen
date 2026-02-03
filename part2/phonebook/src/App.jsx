f3130095d3be8ca62ea88ae7fd3a400aa06c3ca8
Ramzin007 <muhammedramzinp9@gmail.com>
2026-02-03 18:46:35 +0530

    2.7 complete

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -7,14 +7,24 @@ const App = () => {
   const [newName, setNewName] = useState('')
 
   const addPerson = (event) => {
-    event.preventDefault();
+  event.preventDefault()
 
-    const personObject = {
+  const nameExists = persons.some(
+    person => person.name === newName
+  )
+
+  if (nameExists) {
+    alert(`${newName} is already added to phonebook`)
+    return
+  }
+
+  const personObject = {
     name: newName
   }
 
   setPersons(persons.concat(personObject))
   setNewName('')
+  
   } 
 
   return (
