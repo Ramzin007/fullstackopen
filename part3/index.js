e27027fd8e1e23361db00314e063bb6e3153dd11
Ramzin007 <muhammedramzinp9@gmail.com>
2026-02-10 20:59:08 +0530

    exec 3.1 complete

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
-import express, { response } from "express"
+import express, { request, response } from "express"
 
 const app = express();
+
+app.use(express.json())
 
 
 let notes = [
@@ -21,21 +23,31 @@ let notes = [
   }
 ]
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
 
-  if(notes)
-    response.json(note)
-  else
-    response.status(404).end()
-})
+app.post('/api/notes', (request, response) => {
+  const body = request.body
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
 
-  response.status(204).end()
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
 app.get('/api/notes', (request, response) => {
