f29fc207585321574896bc806f5ab96db69f4815
Ramzin007 <muhammedramzinp9@gmail.com>
2026-02-10 22:24:38 +0530

    exec 3.6 complete

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -66,6 +66,22 @@ app.delete('/api/persons/:id' , (req,res) => {
 app.post('/api/persons' , (req,res) => {
     const body = req.body
 
+    if(!body.name || !body.number){
+        return res.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    const nameExist = persons.some(
+        person => person.name === body.name
+    )
+
+    if(nameExist){
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
     const person = {
         id : String(generateId()),
         name : body.name,
