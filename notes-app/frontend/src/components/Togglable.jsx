bd17e9cfa023467e6827c50c9fc93168f5566608
Ramzin007 <muhammedramzinp9@gmail.com>
2026-05-31 15:10:59 +0530

    part 5-6

--- notes-app/frontend/src/components/Togglable.jsx
+++ notes-app/frontend/src/components/Togglable.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 
-const Togglable = props => {
+const Togglable = (props) => {
   const [visible, setVisible] = useState(false)
 
-  const hideWhenVisible = { display: visible ? 'none' : '' }
-  const showWhenVisible = { display: visible ? '' : 'none' }
+  const hideWhenVisible = {
+    display: visible ? 'none' : ''
+  }
+
+  const showWhenVisible = {
+    display: visible ? '' : 'none'
+  }
 
   const toggleVisibility = () => {
     setVisible(!visible)
@@ -13,11 +18,16 @@ const Togglable = props => {
   return (
     <div>
       <div style={hideWhenVisible}>
-        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+        <button onClick={toggleVisibility}>
+          {props.buttonLabel}
+        </button>
       </div>
+
       <div style={showWhenVisible}>
         {props.children}
-        <button onClick={toggleVisibility}>cancel</button>
+        <button onClick={toggleVisibility}>
+          cancel
+        </button>
       </div>
     </div>
   )
