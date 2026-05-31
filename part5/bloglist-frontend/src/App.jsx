e66e3990b4b0f769a6404a34e09d2a29834be9a1
Ramzin007 <muhammedramzinp9@gmail.com>
2026-05-31 11:53:31 +0530

    ex 5.4 initial commit

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -4,10 +4,12 @@ import blogService from './services/blogs'
 import LoginForm from './components/LoginForm'
 import loginService from './services/login'
 import NewBlog from './components/NewBlog'
+import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
+  const [notification, setNotification] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -39,7 +41,10 @@ const App = () => {
     blogService.setToken(user.token)
     setUser(user)
   } catch (exception) {
-    console.log('wrong credentials')
+    setNotification('wrong credentials')
+    setTimeout(() => {
+      setNotification(null)
+    }, 5000)
   }
 }
 
@@ -51,12 +56,17 @@ const handleLogout = () => {
 const addBlog = async blogObject => {
   const returnedBlog = await blogService.create(blogObject)
   setBlogs(blogs.concat(returnedBlog))
+  setNotification(`a new blog ${returnedBlog.title} by ${returnedBlog.author} added`)
+  setTimeout(() => {
+    setNotification(null)
+  }, 5000)
 }
 
     if (user === null) {
     return (
       <div>
         <h2>Log in to application</h2>
+        <Notification message={notification} />
         <LoginForm handleLogin={handleLogin} />
       </div>
     )
@@ -65,6 +75,7 @@ const addBlog = async blogObject => {
   return (
     <div>
       <h2>Blogs</h2>
+      <Notification message={notification} />
       <p>
         {user.name} Logged in
         <button onClick={handleLogout}>
