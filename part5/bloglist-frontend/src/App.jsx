f26d8052e1939f2aa0945b94fb3d24607c4156cc
Ramzin007 <muhammedramzinp9@gmail.com>
2026-05-31 11:03:16 +0530

    ex 5.2 complete

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -42,6 +42,11 @@ const App = () => {
   }
 }
 
+const handleLogout = () => {
+  window.localStorage.removeItem('loggedBlogappUser')
+  setUser(null)
+}
+
     if (user === null) {
     return (
       <div>
@@ -54,7 +59,12 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>
+        {user.name} logged in
+        <button onClick={handleLogout}>
+         logout
+        </button>
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
