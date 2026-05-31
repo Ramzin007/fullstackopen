db56d6f0b9234681538e3eb76859a768e4811d73
Ramzin007 <muhammedramzinp9@gmail.com>
2026-05-31 10:50:10 +0530

    ex 5.1 complete

--- part4/Blog-List/utils/config.js
+++ part4/Blog-List/utils/config.js
@@ -1,10 +1,16 @@
 require('dotenv').config()
 
+const PORT = process.env.PORT
+
 const MONGODB_URI =
   process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI
 
-const PORT = process.env.PORT || 3003
+const SECRET = process.env.SECRET
 
-module.exports = { MONGODB_URI, PORT }
+module.exports = {
+  MONGODB_URI,
+  PORT,
+  SECRET
+}
--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import LoginForm from './components/LoginForm'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +14,24 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async credentials => {
+    const user = await loginService.login(credentials)
+    setUser(user)
+  }
+
+    if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <LoginForm handleLogin={handleLogin} />
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+      <p>{user.name} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
