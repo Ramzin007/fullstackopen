78dccab3d6d3e794466c6f2eb177751a6cf38a01
Ramzin007 <muhammedramzinp9@gmail.com>
2026-05-31 17:23:12 +0530

    ex 5.10 complete

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -134,7 +134,9 @@ const App = () => {
 
       <NewBlog createBlog={addBlog} />
 
-      {blogs.map(blog => (
+      {[...blogs]
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog => (
         <Blog
           key={blog.id}
           blog={blog}
